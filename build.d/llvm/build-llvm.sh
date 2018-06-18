70c012191a68cc7353afebcdcdadc7060a607d83
Yung-Yu Chen <yyc@solvcon.net>
2018-06-18 21:35:44 +0800

    Add libc++ for llvm

--- build.d/llvm/build-llvm.sh
+++ build.d/llvm/build-llvm.sh
@@ -22,6 +22,16 @@ clext_loc=$YHDL/$clext_full.$tarext
 clext_url=http://releases.llvm.org/$version/$clext_full.$tarext
 download $clext_loc $clext_url 6b1d543116dab5a3caba10091d983743
 
+libcxx_full=libcxx-$version.src
+libcxx_loc=$YHDL/$libcxx_full.$tarext
+libcxx_url=http://releases.llvm.org/$version/$libcxx_full.$tarext
+download $libcxx_loc $libcxx_url 4ecad7dfd8ea636205d3ffef028df73a
+
+libcxxabi_full=libcxxabi-$version.src
+libcxxabi_loc=$YHDL/$libcxxabi_full.$tarext
+libcxxabi_url=http://releases.llvm.org/$version/$libcxxabi_full.$tarext
+download $libcxxabi_loc $libcxxabi_url 9d06327892fc5d8acec4ef2e2821ab3d
+
 omp_full=openmp-$version.src
 omp_loc=$YHDL/$omp_full.$tarext
 omp_url=http://releases.llvm.org/$version/$omp_full.$tarext
@@ -45,6 +55,16 @@ mv $clext_full extra
 cd ../../..
 
 cd projects
+tar xf $libcxx_loc
+mv $libcxx_full libcxx
+cd ..
+
+cd projects
+tar xf $libcxxabi_loc
+mv $libcxxabi_full libcxxabi
+cd ..
+
+cd projects
 tar xf $omp_loc
 mv $omp_full openmp
 cd ..
@@ -62,8 +82,13 @@ fi
 cmakecmd=("cmake")
 cmakecmd+=("-DCMAKE_BUILD_TYPE=MinSizeRel")
 cmakecmd+=("-DCMAKE_INSTALL_PREFIX=${INSTALLDIR}")
+cmakecmd+=("-DLLVM_ENABLE_LIBCXX=ON")
+cmakecmd+=("-DLLVM_ENABLE_LIBCXXABI=ON")
+#if [ $(uname) == Linux && "${CXX}" != "clang++" ]; then
+#  cmakecmd+=("-DLIBCXX_CXX_ABI=libstdc++")
+#fi
 
-echo "configuration:"
+echo "configuration: ${cmakecmd[@]}"
 { time "${cmakecmd[@]}" ../.. ; } > cmake.log 2>&1
 echo "configuration done"
 
@@ -72,7 +97,7 @@ echo "build:"
 echo "built"
 
 echo "install:"
-{ time make install ; } > install.log 2>&1
+{ time make -j $NP install ; } > install.log 2>&1
 echo "installation done"
 
 # vim: set et nobomb ff=unix fenc=utf8:
