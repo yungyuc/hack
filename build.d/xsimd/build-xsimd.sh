b2834e5a315c718c84ac6a4ba4f3387b3d7dce7e
Yung-Yu Chen <yyc@solvcon.net>
2018-08-04 23:08:31 +0800

    Add xtensor (including xtl and xsimd) build scripts)

---
+++ build.d/xsimd/build-xsimd.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Copyright (C) 2011 Yung-Yu Chen <yyc@solvcon.net>.
+
+pkgname=xsimd
+pkgbranch=${VERSION:-master}
+pkgfull=$pkgname-$pkgbranch
+pkgrepo=https://github.com/QuantStack/$pkgname.git
+
+# unpack (clone)
+mkdir -p $YHROOT/src/$FLAVOR
+cd $YHROOT/src/$FLAVOR
+if [ ! -d $pkgfull ] ; then
+  git clone -q -b $pkgbranch $pkgrepo $pkgfull
+else
+  cd $pkgfull
+  git co $pkgbranch
+  git pull origin $pkgbranch
+  cd ..
+fi
+cd $pkgfull
+
+if [ -z "$FLAVOR" ] ; then
+  echo "no hacking environment is set"
+  exit 0
+else
+  rm -rf build/hbuild_$FLAVOR
+  mkdir -p build/hbuild_$FLAVOR
+  cd build/hbuild_$FLAVOR
+fi
+
+# build.
+cmakecmd=("cmake")
+cmakecmd+=("-DCMAKE_INSTALL_PREFIX=${INSTALLDIR}")
+cmakecmd+=("-DBUILD_TESTS=OFF")
+if [[ $FLAVOR == opt* ]] ; then
+  cmakecmd+=("-DCMAKE_BUILD_TYPE=Release")
+elif [[ $FLAVOR == dbg* ]] ; then
+  cmakecmd+=("-DCMAKE_BUILD_TYPE=Debug")
+fi
+
+buildcmd cmake.log "${cmakecmd[@]}" ../..
+buildcmd make.log make
+buildcmd install.log make install
+
+# vim: set et nobomb ff=unix fenc=utf8:
