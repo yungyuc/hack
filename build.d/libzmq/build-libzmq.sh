24ed152891b832e82c13e6e02333052691a2800f
Yung-Yu Chen <yyc@solvcon.net>
2019-04-18 08:13:34 +0800

    Update libzmq

--- build.d/libzmq/build-libzmq.sh
+++ build.d/libzmq/build-libzmq.sh
@@ -14,16 +14,60 @@ download $zmqloc $zmqurl da43d89dac623d99909fb95e2725fe05
 mkdir -p $YHROOT/src/$FLAVOR
 cd $YHROOT/src/$FLAVOR
 tar xf $zmqloc
-mkdir -p $zmqfull/build
-cd $zmqfull/build
+cd $zmqfull
 
-# build.
-{ time cmake \
-  -DCMAKE_PREFIX_PATH=$INSTALLDIR \
-  -DCMAKE_INSTALL_PREFIX=$INSTALLDIR \
-  -DCMAKE_INSTALL_LIBDIR=lib \
-  .. ; } > cmake.log 2>&1
-{ make -j $NP VERBOSE=1 ; } > make.log 2>&1
-{ make install ; } > install.log 2>&1
+# build again using autotool
+if [[ `uname` == Darwin ]]; then
+  export LDFLAGS="-Wl,-rpath,$PREFIX/lib $LDFLAGS"
+fi
+
+cfgcmd=("./configure")
+cfgcmd+=("--prefix=${INSTALLDIR}")
+
+buildcmd autogen.log ./autogen.sh
+buildcmd configure.log "${cfgcmd[@]}"
+buildcmd make.log make -j
+buildcmd install.log make install
+
+# Generate CMake files, so downstream packages can use `find_package(ZeroMQ)`,
+# which is normally only available when libzmq is itself installed with CMake
+
+CMAKE_DIR="${INSTALLDIR}/lib/cmake/ZeroMQ"
+mkdir -p "$CMAKE_DIR"
+
+cat << EOF > "$CMAKE_DIR/ZeroMQConfig.cmake"
+set(PN ZeroMQ)
+set(_YH_PREFIX "$INSTALLDIR")
+set(\${PN}_INCLUDE_DIR "\${_YH_PREFIX}/include")
+set(\${PN}_LIBRARY "\${_YH_PREFIX}/lib/libzmq\${CMAKE_STATIC_LIBRARY_SUFFIX}")
+set(\${PN}_STATIC_LIBRARY "\${_YH_PREFIX}/lib/libzmq.a")
+unset(_YH_PREFIX)
+set(\${PN}_FOUND TRUE)
+# add libzmq-$zmqver cmake targets
+# only define targets once
+# this file can be loaded multiple times
+if (TARGET libzmq)
+  return()
+endif()
+add_library(libzmq SHARED IMPORTED)
+set_property(TARGET libzmq PROPERTY INTERFACE_INCLUDE_DIRECTORIES "\${\${PN}_INCLUDE_DIR}")
+set_property(TARGET libzmq PROPERTY IMPORTED_LOCATION "\${\${PN}_LIBRARY}")
+add_library(libzmq-static STATIC IMPORTED "\${\${PN}_INCLUDE_DIR}")
+set_property(TARGET libzmq-static PROPERTY INTERFACE_INCLUDE_DIRECTORIES "\${\${PN}_INCLUDE_DIR}")
+set_property(TARGET libzmq-static PROPERTY IMPORTED_LOCATION "\${\${PN}_STATIC_LIBRARY}")
+EOF
+
+cat << EOF > "$CMAKE_DIR/ZeroMQConfigVersion.cmake"
+set(PACKAGE_VERSION "$PKG_VERSION")
+# Check whether the requested PACKAGE_FIND_VERSION is compatible
+if("\${PACKAGE_VERSION}" VERSION_LESS "\${PACKAGE_FIND_VERSION}")
+  set(PACKAGE_VERSION_COMPATIBLE FALSE)
+else()
+  set(PACKAGE_VERSION_COMPATIBLE TRUE)
+  if ("\${PACKAGE_VERSION}" VERSION_EQUAL "\${PACKAGE_FIND_VERSION}")
+    set(PACKAGE_VERSION_EXACT TRUE)
+  endif()
+endif()
+EOF
 
 # vim: set et nobomb ff=unix fenc=utf8:
