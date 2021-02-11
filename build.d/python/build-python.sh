bb8b0bdeb43ddfd40df3024517021ec0e371e157
Yung-Yu Chen <yyc@solvcon.net>
2021-02-11 08:07:17 +0800

    [python] Upgrade to 3.9 on macos 11 (Big Sur)

--- build.d/python/build-python.sh
+++ build.d/python/build-python.sh
@@ -5,7 +5,7 @@
 # Python development guide: https://docs.python.org/devguide/
 
 pkgname=python
-pkgbranch=${VERSION:-3.7}
+pkgbranch=${VERSION:-master}
 pkgfull=$pkgname-$pkgbranch
 pkgrepo=https://github.com/python/cpython.git
 
@@ -16,8 +16,10 @@ if [ ! -d $pkgfull ] ; then
   git clone -q -b $pkgbranch $pkgrepo $pkgfull
 else
   cd $pkgfull
-  git co $pkgbranch
-  git pull origin $pkgbranch
+  if [ -z "$NOPULL" ] ; then
+    git co $pkgbranch
+    git pull origin $pkgbranch
+  fi
   cd ..
 fi
 cd $pkgfull
@@ -32,7 +34,7 @@ mkdir -p $PREFIX/include
 
 if [ $(uname) == Darwin ]; then
   export CPPFLAGS="-I$PREFIX/include -I$brewssldir/include $CPPFLAGS"
-  export LDFLAGS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -L$brewssldir/lib -headerpad_max_install_names $LDFLAGS"
+  export LDFLAGS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -L$brewssldir/lib $LDFLAGS"
   sed -i -e "s/@OSX_ARCH@/$ARCH/g" Lib/distutils/unixccompiler.py
 elif [ $(uname) == Linux ]; then
   export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
@@ -44,9 +46,14 @@ cfgcmd+=("--prefix=$PREFIX")
 cfgcmd+=("--enable-shared")
 cfgcmd+=("--enable-ipv6")
 cfgcmd+=("--with-ensurepip=no")
+cfgcmd+=("--enable-optimizations")
+cfgcmd+=("--with-lto")
 cfgcmd+=("--with-tcltk-includes=-I$PREFIX/include")
 cfgcmd+=("--with-tcltk-libs=\"-L$PREFIX/lib -ltcl8.5 -ltk8.5\"")
-if [[ $(uname) == Darwin ]] && [[ $VERSION == 3.7* ]] ; then
+if [[ $(uname) == Darwin ]] ; then
+  #cfgcmd+=("--enable-universalsdk")
+  #cfgcmd+=("--with-universal-archs=intel-64")
+  #cfgcmd+=("LLVM_AR=/usr/bin/ar")
   cfgcmd+=("--with-openssl=$brewssldir")
 fi
 #cfgcmd+=("--enable-loadable-sqlite-extensions")
